6d71af97655150239a798b0e7056d9f0f5f990e2
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 22:31:34 -0300

    26/03 Início e desenvolvimento do projeto ironkey

--- ironkey/lib/main.dart
+++ ironkey/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ironkey/app_theme.dart';
 
 void main() {
@@ -11,7 +14,7 @@ class IronKeyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
@@ -28,8 +31,100 @@ class IronKeyScreen extends StatefulWidget {
 }
 
 class _IronKeyScreenState extends State<IronKeyScreen> {
+  final TextEditingController _passwordController =
+      TextEditingController(); //criação de uma variavel com _ significa que ela é privada
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void copyPassword(String password) {
+    Clipboard.setData(ClipboardData(text: password));
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text('Senha copiada!')));
+  }
+
+  void generatePassword() {
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#\$%&*";
+    final chars = upper + lower + numbers + symbols;
+    final random = Random();
+    setState(() {
+      _passwordController.text = List.generate(
+        12,
+        (_) => chars[random.nextInt(chars.length)],
+      ).join();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  ClipOval(
+                    child: SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: Image.asset(
+                        "assets/images/ironman.jpg",
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "Sua senha segura",
+                    textAlign: TextAlign.center,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: _passwordController.text.isNotEmpty
+                          ? IconButton(
+                              onPressed: () {
+                                copyPassword(_passwordController.text);
+                              },
+                              icon: Icon(Icons.copy),
+                            )
+                          : null,
+                    ),
+                  ),
+                  Text(_passwordController.text),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(onPressed: () {}, child: Text("Gerar senha")),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
