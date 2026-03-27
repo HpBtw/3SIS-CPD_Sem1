6413d195b30f5c39118f1cb4dbff0f8f783f1757
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 21:38:47 -0300

    ironkey first commit

--- ironkey/lib/main.dart
+++ ironkey/lib/main.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:ironkey/app_theme.dart';
 
 void main() {
-  runApp(IronKeyApp);
+  runApp(IronKeyApp());
 }
 
+class IronKeyApp extends StatelessWidget {
+  const IronKeyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: IronKeyScreen(),
+    );
+  }
+}
+
+class IronKeyScreen extends StatefulWidget {
+  const IronKeyScreen({super.key});
+
+  @override
+  State<IronKeyScreen> createState() => _IronKeyScreenState();
+}
+
+class _IronKeyScreenState extends State<IronKeyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
